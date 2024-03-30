3910c7b061c8ea33b23181b5c2401ae32fda9ae7
Uday032 <rahul.uday07@gmail.com>
2024-03-30 14:03:40 +0530

    Updated nextconfig

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: "cdn.remotual.com",
         port: "",
       },
+      {
+        protocol: 'https',
+        hostname: 'salesdeck.c10oc6moksyd.us-east-1.rds.amazonaws.com',
+        port: '3306',
+      },
     ],
   },
 };
